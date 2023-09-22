8b54947513c38e1ea4c17caecce0465458d6b76b
Deyvi Condori <deivcondor@gmail.com>
2023-09-21 23:40:35 -0500

    Agregar archivos de proyecto.

---
+++ Laboratorio 3 - Semana 5/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace PromNotas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingrese el número de notas: ");
+            int cantidadNotas = int.Parse(Console.ReadLine());
+
+            int sumaNotas = 0; 
+
+            for (int i = 1; i <= cantidadNotas; i++)
+            {
+                Console.Write($"Ingrese la nota {i}: ");
+                int nota = int.Parse(Console.ReadLine());
+                sumaNotas += nota;
+            }
+
+            int promedio = sumaNotas / cantidadNotas;
+
+            Console.WriteLine($"El promedio es {promedio}");
+        }
+    }
+}
